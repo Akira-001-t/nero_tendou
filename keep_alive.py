cccdae5c951742cf51dd8c8df0328fc69eabfba4
Akira-001-t <tendou.akira1945@gmail.com>
2025-08-19 08:28:15 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -32,7 +32,7 @@ def ping():
 
 def run():
     """Run the Flask server"""
-    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
+    port = int(os.environ.get("PORT", 5000))  # Render uses $PORT
     app.run(host="0.0.0.0", port=port, debug=False)
 
 def keep_alive():
